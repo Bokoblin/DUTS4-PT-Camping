35df95eee236406936eb5197340872900e95896c
Arthur Jolivet <jolivet.arthur@gmail.com>
2017-02-04 14:10:41 +0100

    Added stub winforms for "Connexion" and "Home", added changelog

---
+++ PT_Camping/Connexion.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace PT_Camping
+{
+    partial class Connexion
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.labelMenuStrip = new System.Windows.Forms.Label();
+            this.userTextBox = new System.Windows.Forms.TextBox();
+            this.passwordTextBox = new System.Windows.Forms.TextBox();
+            this.userLabel = new System.Windows.Forms.Label();
+            this.passLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(346, 400);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(91, 30);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Entrer";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.boutonEntrer_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.AutoSize = false;
+            this.menuStrip1.BackColor = System.Drawing.Color.RoyalBlue;
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(784, 50);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // labelMenuStrip
+            // 
+            this.labelMenuStrip.AutoSize = true;
+            this.labelMenuStrip.BackColor = System.Drawing.Color.RoyalBlue;
+            this.labelMenuStrip.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelMenuStrip.ForeColor = System.Drawing.Color.White;
+            this.labelMenuStrip.Location = new System.Drawing.Point(40, 15);
+            this.labelMenuStrip.Name = "labelMenuStrip";
+            this.labelMenuStrip.Size = new System.Drawing.Size(84, 20);
+            this.labelMenuStrip.TabIndex = 2;
+            this.labelMenuStrip.Text = "Connexion";
+            // 
+            // userTextBox
+            // 
+            this.userTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.userTextBox.Location = new System.Drawing.Point(315, 188);
+            this.userTextBox.MinimumSize = new System.Drawing.Size(0, 30);
+            this.userTextBox.Name = "userTextBox";
+            this.userTextBox.Size = new System.Drawing.Size(160, 30);
+            this.userTextBox.TabIndex = 3;
+            // 
+            // passwordTextBox
+            // 
+            this.passwordTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.passwordTextBox.Location = new System.Drawing.Point(315, 253);
+            this.passwordTextBox.MinimumSize = new System.Drawing.Size(0, 30);
+            this.passwordTextBox.Name = "passwordTextBox";
+            this.passwordTextBox.Size = new System.Drawing.Size(160, 30);
+            this.passwordTextBox.TabIndex = 5;
+            this.passwordTextBox.UseSystemPasswordChar = true;
+            // 
+            // userLabel
+            // 
+            this.userLabel.AutoSize = true;
+            this.userLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.userLabel.Location = new System.Drawing.Point(184, 188);
+            this.userLabel.Name = "userLabel";
+            this.userLabel.Size = new System.Drawing.Size(80, 20);
+            this.userLabel.TabIndex = 6;
+            this.userLabel.Text = "Identifiant";
+            // 
+            // passLabel
+            // 
+            this.passLabel.AutoSize = true;
+            this.passLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.passLabel.Location = new System.Drawing.Point(184, 253);
+            this.passLabel.Name = "passLabel";
+            this.passLabel.Size = new System.Drawing.Size(105, 20);
+            this.passLabel.TabIndex = 7;
+            this.passLabel.Text = "Mot de passe";
+            // 
+            // Connexion
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.passLabel);
+            this.Controls.Add(this.userLabel);
+            this.Controls.Add(this.passwordTextBox);
+            this.Controls.Add(this.userTextBox);
+            this.Controls.Add(this.labelMenuStrip);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Connexion";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "PT4-BJJMR";
+            this.Load += new System.EventHandler(this.Connexion_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.Label labelMenuStrip;
+        private System.Windows.Forms.TextBox userTextBox;
+        private System.Windows.Forms.TextBox passwordTextBox;
+        private System.Windows.Forms.Label userLabel;
+        private System.Windows.Forms.Label passLabel;
+    }
+}
+
---
+++ PT_Camping/Connexion.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PT_Camping
+{
+    /// <summary>
+    /// Fenêtre de connexion à l'application.
+    /// L'accès à l'application ne se fait que si le nom d'utilisateur 
+    /// et le mot de passe sont valides.
+    /// </summary>
+    public partial class Connexion : Form
+    {
+        public Connexion()
+        {
+            this.StartPosition = FormStartPosition.CenterScreen;
+            InitializeComponent();
+        }
+
+        private void Connexion_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void boutonEntrer_Click(object sender, EventArgs e)
+        {
+            //stub -- à implémenter
+            Accueil accueil = new Accueil();
+            accueil.Width = this.Width;
+            accueil.Height = this.Height;
+            accueil.StartPosition = this.StartPosition;
+            accueil.Show();
+            this.Hide();
+        }
+    }
+}
