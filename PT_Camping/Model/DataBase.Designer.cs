446f967428000dbd632b10e21a498c9dd4a74c9b
ajacquot <jacquot.alexandre97@orange.fr>
2017-02-12 03:17:55 +0100

    changing dataBase for MySQL

--- PT_Camping/Model/DataBase.Designer.cs
+++ PT_Camping/Model/DataBase.Designer.cs
@@ -1,4 +1,4 @@
-﻿// La génération de code T4 est activée pour le modèle 'V:\Cours\Projet\Projet Camping\DUTS4-PT-Camping\PT_Camping\Model\BDD_Camping.edmx'. 
+﻿// La génération de code T4 est activée pour le modèle 'D:\Cours\Projet\Projet Camping\DUTS4-PT-Camping\PT_Camping\Model\DataBase.edmx'. 
 // Pour activer la génération de code héritée, définissez la valeur de la propriété
 // du concepteur 'Stratégie de génération de code' sur 'ObjectContext hérité'. Cette propriété est disponible dans la fenêtre Propriétés lorsque le modèle
 //  est ouvert dans le concepteur.
