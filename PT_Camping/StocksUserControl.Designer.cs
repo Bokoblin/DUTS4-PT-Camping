1a94f8b68b24df2cc6ee7993023b657a694a450f
yonnel <yoyo33220@gmail.com>
2017-02-13 12:02:25 +0100

    DataGridViewStock beginning

--- PT_Camping/StocksUserControl.Designer.cs
+++ PT_Camping/StocksUserControl.Designer.cs
@@ -28,10 +28,40 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            this.DataGridViewProduct = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.DataGridViewProduct)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // logoutButton
+            // 
+            this.logoutButton.FlatAppearance.BorderColor = System.Drawing.Color.RoyalBlue;
+            this.logoutButton.FlatAppearance.BorderSize = 0;
+            // 
+            // DataGridViewProduct
+            // 
+            this.DataGridViewProduct.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DataGridViewProduct.Location = new System.Drawing.Point(26, 78);
+            this.DataGridViewProduct.Name = "DataGridViewProduct";
+            this.DataGridViewProduct.Size = new System.Drawing.Size(320, 466);
+            this.DataGridViewProduct.TabIndex = 10;
+            // 
+            // StocksUserControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.DataGridViewProduct);
+            this.Name = "StocksUserControl";
+            this.Controls.SetChildIndex(this.appBarTitle, 0);
+            this.Controls.SetChildIndex(this.logoutButton, 0);
+            this.Controls.SetChildIndex(this.DataGridViewProduct, 0);
+            ((System.ComponentModel.ISupportInitialize)(this.DataGridViewProduct)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView DataGridViewProduct;
     }
 }
