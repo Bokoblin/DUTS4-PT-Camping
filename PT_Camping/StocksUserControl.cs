a0980d9b9e0e7a082791059cffc945b351464a7b
yonnel <yoyo33220@gmail.com>
2017-02-15 15:55:05 +0100

    fezf

--- PT_Camping/StocksUserControl.cs
+++ PT_Camping/StocksUserControl.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using PT_Camping.Model;
+using System.Data.Entity.Core.Objects;
 
 namespace PT_Camping
 {
@@ -37,14 +38,41 @@ namespace PT_Camping
             database.Produit.Add(p);
             database.SaveChanges();
             */
-            //DataGridView
             //SaveChanges()
-            DataGridViewProduct.ReadOnly = true;
-            DataGridViewProduct.DataSource = database.Produit.ToList();
-            DataGridViewProduct.Columns["Code_Produit"].Visible = false;
-            DataGridViewProduct.Columns["A_Vendu"].Visible = false;
-            DataGridViewProduct.Columns["Fournisseur"].Visible = false;
-            MessageBox.Show("There are : " + database.Produit.Count().ToString() + " products in store.");
+            /*
+            MessageBox.Show("There are : " + database.Produit.Count().ToString() + " products in store.");*/
+            //var query = from produit in database.Produit select produit.Libelle_Produit;
+
+            foreach (var result in database.Produit )
+            {
+                ProductListView.Items.Add(result.Libelle_Produit);
+            }
+            //ProductDataGridView.DataSource = query.ToList();
+        }
+
+        private void buttonAddStock_MouseClick(object sender, MouseEventArgs e)
+        {
+            Panel dynamicPanel = new Panel();
+            dynamicPanel.Location = new System.Drawing.Point(26, 12);
+            dynamicPanel.Name = "Panel1";
+            dynamicPanel.Size = new System.Drawing.Size(228, 200);
+            dynamicPanel.BackColor = Color.LightBlue;
+
+
+            TextBox textBox1 = new TextBox();
+            textBox1.Location = new Point(10, 10);
+            textBox1.Text = "I am a TextBox5";
+            textBox1.Size = new Size(200, 30);
+
+            CheckBox checkBox1 = new CheckBox();
+            checkBox1.Location = new Point(10, 50);
+            checkBox1.Text = "Check Me";
+            checkBox1.Size = new Size(200, 30);
+
+            dynamicPanel.Controls.Add(textBox1);
+            dynamicPanel.Controls.Add(checkBox1);
+
+            Controls.Add(dynamicPanel);
         }
     }
 }
