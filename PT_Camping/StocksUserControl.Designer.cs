8e3ba6d258494a0c66f449012559ea09413fe2c2
yonnel <yoyo33220@gmail.com>
2017-02-15 15:55:05 +0100

    fezf

--- PT_Camping/StocksUserControl.Designer.cs
+++ PT_Camping/StocksUserControl.Designer.cs
@@ -28,8 +28,9 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.DataGridViewProduct = new System.Windows.Forms.DataGridView();
-            ((System.ComponentModel.ISupportInitialize)(this.DataGridViewProduct)).BeginInit();
+            this.buttonAddStock = new System.Windows.Forms.Button();
+            this.ProductListView = new System.Windows.Forms.ListView();
+            this.Nom_Produit = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.SuspendLayout();
             // 
             // logoutButton
@@ -37,31 +38,47 @@
             this.logoutButton.FlatAppearance.BorderColor = System.Drawing.Color.RoyalBlue;
             this.logoutButton.FlatAppearance.BorderSize = 0;
             // 
-            // DataGridViewProduct
+            // buttonAddStock
             // 
-            this.DataGridViewProduct.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.DataGridViewProduct.Location = new System.Drawing.Point(26, 78);
-            this.DataGridViewProduct.Name = "DataGridViewProduct";
-            this.DataGridViewProduct.Size = new System.Drawing.Size(320, 466);
-            this.DataGridViewProduct.TabIndex = 10;
+            this.buttonAddStock.Location = new System.Drawing.Point(143, 482);
+            this.buttonAddStock.Name = "buttonAddStock";
+            this.buttonAddStock.Size = new System.Drawing.Size(109, 23);
+            this.buttonAddStock.TabIndex = 11;
+            this.buttonAddStock.Text = "Nouveau Stock";
+            this.buttonAddStock.UseVisualStyleBackColor = true;
+            this.buttonAddStock.MouseClick += new System.Windows.Forms.MouseEventHandler(this.buttonAddStock_MouseClick);
+            // 
+            // ProductListView
+            // 
+            this.ProductListView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.Nom_Produit});
+            this.ProductListView.GridLines = true;
+            this.ProductListView.Location = new System.Drawing.Point(93, 109);
+            this.ProductListView.Name = "ProductListView";
+            this.ProductListView.Size = new System.Drawing.Size(178, 318);
+            this.ProductListView.TabIndex = 12;
+            this.ProductListView.UseCompatibleStateImageBehavior = false;
+            this.ProductListView.View = System.Windows.Forms.View.List;
             // 
             // StocksUserControl
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.DataGridViewProduct);
+            this.Controls.Add(this.ProductListView);
+            this.Controls.Add(this.buttonAddStock);
             this.Name = "StocksUserControl";
+            this.Controls.SetChildIndex(this.buttonAddStock, 0);
+            this.Controls.SetChildIndex(this.ProductListView, 0);
             this.Controls.SetChildIndex(this.appBarTitle, 0);
             this.Controls.SetChildIndex(this.logoutButton, 0);
-            this.Controls.SetChildIndex(this.DataGridViewProduct, 0);
-            ((System.ComponentModel.ISupportInitialize)(this.DataGridViewProduct)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.DataGridView DataGridViewProduct;
+        private System.Windows.Forms.Button buttonAddStock;
+        private System.Windows.Forms.ListView ProductListView;
+        private System.Windows.Forms.ColumnHeader Nom_Produit;
     }
 }
