8ac5b964c0e2e8559010e271eabcf1954c604e25
Valentine2009 <val20_09@live.fr>
2017-02-27 12:19:18 +0100

    v1

--- PT_Camping/View/UserControls/ClientsUserControl.cs
+++ PT_Camping/View/UserControls/ClientsUserControl.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using PT_Camping.Model;
 
 namespace PT_Camping
 {
@@ -19,11 +20,41 @@ namespace PT_Camping
     /// Since : 08/02/17
     public partial class ClientsUserControl : ManagementUserControl
     {
+        private DataBase db;
         public ClientsUserControl(HomeUserControl homeUserControl) : base(homeUserControl)
         {
             InitializeComponent();
             appBarTitle.Text = "Gestion des clients";
+            db = new DataBase();
+
+            ClientlistView.View = View.Details;
+            ClientlistView.Columns.Add("Nom", -2);
+            ClientlistView.Columns.Add("Prénom", -2);
+
+           updateClientlistView();
             handleResize();
+        }
+
+        private void updateClientlistView()
+        {
+            ClientlistView.Items.Clear();
+
+            foreach (var cl in db.Client)
+            {
+                
+                    string surname = cl.Personne.Nom_Personne.ToUpper();
+                    string name = cl.Personne.Prenom_Personne;
+
+                    var item = new ListViewItem(new[] { surname, name });
+                    item.Name = cl.Code_Personne.ToString();
+                    ClientlistView.Items.Add(item);    
+            }
+
+            if (ClientlistView.Items.Count > 0)
+            {
+                ClientlistView.Items[0].Selected = true;
+                ClientlistView.Select();
+            }
         }
     }
 }
