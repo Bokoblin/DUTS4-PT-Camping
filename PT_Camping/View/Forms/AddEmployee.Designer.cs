7d2759951e66bc616de75c36903de5abfc449d0d
Valentine2009 <val20_09@live.fr>
2017-03-08 13:25:06 +0100

    version addClient

--- PT_Camping/View/Forms/AddEmployee.Designer.cs
+++ PT_Camping/View/Forms/AddEmployee.Designer.cs
@@ -54,86 +54,97 @@
             // 
             // nameTextBox
             // 
-            this.nameTextBox.Location = new System.Drawing.Point(56, 77);
+            this.nameTextBox.Location = new System.Drawing.Point(75, 95);
+            this.nameTextBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.nameTextBox.Name = "nameTextBox";
-            this.nameTextBox.Size = new System.Drawing.Size(156, 20);
+            this.nameTextBox.Size = new System.Drawing.Size(207, 22);
             this.nameTextBox.TabIndex = 2;
             // 
             // nameLabel
             // 
-            this.nameLabel.Location = new System.Drawing.Point(12, 54);
+            this.nameLabel.Location = new System.Drawing.Point(16, 66);
+            this.nameLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.nameLabel.Name = "nameLabel";
-            this.nameLabel.Size = new System.Drawing.Size(240, 20);
+            this.nameLabel.Size = new System.Drawing.Size(320, 25);
             this.nameLabel.TabIndex = 33;
             this.nameLabel.Text = "Prénom*";
             this.nameLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // loginTextBox
             // 
-            this.loginTextBox.Location = new System.Drawing.Point(56, 327);
+            this.loginTextBox.Location = new System.Drawing.Point(75, 402);
+            this.loginTextBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.loginTextBox.Name = "loginTextBox";
-            this.loginTextBox.Size = new System.Drawing.Size(156, 20);
+            this.loginTextBox.Size = new System.Drawing.Size(207, 22);
             this.loginTextBox.TabIndex = 7;
             // 
             // emailTextBox
             // 
-            this.emailTextBox.Location = new System.Drawing.Point(56, 281);
+            this.emailTextBox.Location = new System.Drawing.Point(75, 346);
+            this.emailTextBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.emailTextBox.Name = "emailTextBox";
-            this.emailTextBox.Size = new System.Drawing.Size(156, 20);
+            this.emailTextBox.Size = new System.Drawing.Size(207, 22);
             this.emailTextBox.TabIndex = 6;
             // 
             // phoneTextBox
             // 
-            this.phoneTextBox.Location = new System.Drawing.Point(56, 169);
+            this.phoneTextBox.Location = new System.Drawing.Point(75, 208);
+            this.phoneTextBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.phoneTextBox.Name = "phoneTextBox";
-            this.phoneTextBox.Size = new System.Drawing.Size(156, 20);
+            this.phoneTextBox.Size = new System.Drawing.Size(207, 22);
             this.phoneTextBox.TabIndex = 4;
             // 
             // addressTextBox
             // 
-            this.addressTextBox.Location = new System.Drawing.Point(56, 215);
+            this.addressTextBox.Location = new System.Drawing.Point(75, 265);
+            this.addressTextBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.addressTextBox.Multiline = true;
             this.addressTextBox.Name = "addressTextBox";
-            this.addressTextBox.Size = new System.Drawing.Size(156, 40);
+            this.addressTextBox.Size = new System.Drawing.Size(207, 48);
             this.addressTextBox.TabIndex = 5;
             // 
             // birthDateTextBox
             // 
-            this.birthDateTextBox.Location = new System.Drawing.Point(56, 123);
+            this.birthDateTextBox.Location = new System.Drawing.Point(75, 151);
+            this.birthDateTextBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.birthDateTextBox.Name = "birthDateTextBox";
-            this.birthDateTextBox.Size = new System.Drawing.Size(156, 20);
+            this.birthDateTextBox.Size = new System.Drawing.Size(207, 22);
             this.birthDateTextBox.TabIndex = 3;
             // 
             // surnameTextBox
             // 
-            this.surnameTextBox.Location = new System.Drawing.Point(56, 32);
+            this.surnameTextBox.Location = new System.Drawing.Point(75, 39);
+            this.surnameTextBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.surnameTextBox.Name = "surnameTextBox";
-            this.surnameTextBox.Size = new System.Drawing.Size(156, 20);
+            this.surnameTextBox.Size = new System.Drawing.Size(207, 22);
             this.surnameTextBox.TabIndex = 1;
             // 
             // loginLabel
             // 
-            this.loginLabel.Location = new System.Drawing.Point(12, 304);
+            this.loginLabel.Location = new System.Drawing.Point(16, 374);
+            this.loginLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.loginLabel.Name = "loginLabel";
-            this.loginLabel.Size = new System.Drawing.Size(240, 20);
+            this.loginLabel.Size = new System.Drawing.Size(320, 25);
             this.loginLabel.TabIndex = 26;
             this.loginLabel.Text = "Login*";
             this.loginLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // emailLabel
             // 
-            this.emailLabel.Location = new System.Drawing.Point(12, 258);
+            this.emailLabel.Location = new System.Drawing.Point(16, 318);
+            this.emailLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.emailLabel.Name = "emailLabel";
-            this.emailLabel.Size = new System.Drawing.Size(240, 20);
+            this.emailLabel.Size = new System.Drawing.Size(320, 25);
             this.emailLabel.TabIndex = 25;
             this.emailLabel.Text = "Email*";
             this.emailLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // phoneLabel
             // 
-            this.phoneLabel.Location = new System.Drawing.Point(12, 146);
+            this.phoneLabel.Location = new System.Drawing.Point(16, 180);
+            this.phoneLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.phoneLabel.Name = "phoneLabel";
-            this.phoneLabel.Size = new System.Drawing.Size(240, 20);
+            this.phoneLabel.Size = new System.Drawing.Size(320, 25);
             this.phoneLabel.TabIndex = 24;
             this.phoneLabel.Text = "Téléphone";
             this.phoneLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -142,18 +153,20 @@
             // 
             this.addressLabel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.addressLabel.Location = new System.Drawing.Point(12, 192);
+            this.addressLabel.Location = new System.Drawing.Point(16, 236);
+            this.addressLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.addressLabel.Name = "addressLabel";
-            this.addressLabel.Size = new System.Drawing.Size(240, 20);
+            this.addressLabel.Size = new System.Drawing.Size(320, 25);
             this.addressLabel.TabIndex = 23;
             this.addressLabel.Text = "Adresse";
             this.addressLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // birthDateLabel
             // 
-            this.birthDateLabel.Location = new System.Drawing.Point(12, 100);
+            this.birthDateLabel.Location = new System.Drawing.Point(16, 123);
+            this.birthDateLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.birthDateLabel.Name = "birthDateLabel";
-            this.birthDateLabel.Size = new System.Drawing.Size(240, 20);
+            this.birthDateLabel.Size = new System.Drawing.Size(320, 25);
             this.birthDateLabel.TabIndex = 22;
             this.birthDateLabel.Text = "Date de Naissance*";
             this.birthDateLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -161,36 +174,40 @@
             // surnameLabel
             // 
             this.surnameLabel.ImageAlign = System.Drawing.ContentAlignment.BottomCenter;
-            this.surnameLabel.Location = new System.Drawing.Point(12, 9);
+            this.surnameLabel.Location = new System.Drawing.Point(16, 11);
+            this.surnameLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.surnameLabel.Name = "surnameLabel";
-            this.surnameLabel.Size = new System.Drawing.Size(240, 20);
+            this.surnameLabel.Size = new System.Drawing.Size(320, 25);
             this.surnameLabel.TabIndex = 21;
             this.surnameLabel.Text = "Nom*";
             this.surnameLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // passwordLabel
             // 
-            this.passwordLabel.Location = new System.Drawing.Point(12, 350);
+            this.passwordLabel.Location = new System.Drawing.Point(16, 431);
+            this.passwordLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.passwordLabel.Name = "passwordLabel";
-            this.passwordLabel.Size = new System.Drawing.Size(240, 20);
+            this.passwordLabel.Size = new System.Drawing.Size(320, 25);
             this.passwordLabel.TabIndex = 35;
             this.passwordLabel.Text = "Mot de passe*";
             this.passwordLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // passwordTextBox
             // 
-            this.passwordTextBox.Location = new System.Drawing.Point(56, 373);
+            this.passwordTextBox.Location = new System.Drawing.Point(75, 459);
+            this.passwordTextBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.passwordTextBox.Name = "passwordTextBox";
-            this.passwordTextBox.Size = new System.Drawing.Size(156, 20);
+            this.passwordTextBox.Size = new System.Drawing.Size(207, 22);
             this.passwordTextBox.TabIndex = 8;
             this.passwordTextBox.UseSystemPasswordChar = true;
             // 
             // permissionButton
             // 
             this.permissionButton.AutoSize = true;
-            this.permissionButton.Location = new System.Drawing.Point(72, 438);
+            this.permissionButton.Location = new System.Drawing.Point(96, 539);
+            this.permissionButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.permissionButton.Name = "permissionButton";
-            this.permissionButton.Size = new System.Drawing.Size(120, 30);
+            this.permissionButton.Size = new System.Drawing.Size(160, 37);
             this.permissionButton.TabIndex = 9;
             this.permissionButton.Text = "Permissions";
             this.permissionButton.UseVisualStyleBackColor = true;
@@ -198,9 +215,10 @@
             // 
             // cancelButton
             // 
-            this.cancelButton.Location = new System.Drawing.Point(27, 474);
+            this.cancelButton.Location = new System.Drawing.Point(36, 583);
+            this.cancelButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.cancelButton.Name = "cancelButton";
-            this.cancelButton.Size = new System.Drawing.Size(80, 25);
+            this.cancelButton.Size = new System.Drawing.Size(107, 31);
             this.cancelButton.TabIndex = 10;
             this.cancelButton.Text = "Annuler";
             this.cancelButton.UseVisualStyleBackColor = true;
@@ -208,9 +226,10 @@
             // 
             // OKButton
             // 
-            this.OKButton.Location = new System.Drawing.Point(157, 474);
+            this.OKButton.Location = new System.Drawing.Point(209, 583);
+            this.OKButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.OKButton.Name = "OKButton";
-            this.OKButton.Size = new System.Drawing.Size(80, 25);
+            this.OKButton.Size = new System.Drawing.Size(107, 31);
             this.OKButton.TabIndex = 11;
             this.OKButton.Text = "Valider";
             this.OKButton.UseVisualStyleBackColor = true;
@@ -219,17 +238,18 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(20, 410);
+            this.label1.Location = new System.Drawing.Point(27, 505);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(225, 13);
+            this.label1.Size = new System.Drawing.Size(304, 17);
             this.label1.TabIndex = 40;
             this.label1.Text = "Les valeurs marquées d\'une * sont obligatoires";
             // 
             // AddEmployee
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(264, 511);
+            this.ClientSize = new System.Drawing.Size(352, 629);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.OKButton);
             this.Controls.Add(this.cancelButton);
@@ -251,6 +271,7 @@
             this.Controls.Add(this.addressLabel);
             this.Controls.Add(this.birthDateLabel);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.MaximizeBox = false;
             this.Name = "AddEmployee";
             this.ShowIcon = false;
