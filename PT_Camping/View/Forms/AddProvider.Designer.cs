3bcedcb1cd5c8114deefcfbcf85af6f94d46a62d
clairrouf <clairesakura@live.fr>
2017-03-13 15:11:22 +0100

    Added Providers (PR#10)

---
+++ PT_Camping/View/Forms/AddProvider.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace PT_Camping.View.Forms
+{
+    partial class AddProvider
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.criticalityLabel = new System.Windows.Forms.Label();
+            this.nameP = new System.Windows.Forms.TextBox();
+            this.mailP = new System.Windows.Forms.TextBox();
+            this.adresseP = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.webP = new System.Windows.Forms.TextBox();
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.OKButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // criticalityLabel
+            // 
+            this.criticalityLabel.Location = new System.Drawing.Point(12, 9);
+            this.criticalityLabel.Name = "criticalityLabel";
+            this.criticalityLabel.Size = new System.Drawing.Size(240, 20);
+            this.criticalityLabel.TabIndex = 38;
+            this.criticalityLabel.Text = "Nom du fournisseur";
+            this.criticalityLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // nameP
+            // 
+            this.nameP.Location = new System.Drawing.Point(57, 32);
+            this.nameP.Name = "nameP";
+            this.nameP.Size = new System.Drawing.Size(156, 20);
+            this.nameP.TabIndex = 39;
+            // 
+            // mailP
+            // 
+            this.mailP.Location = new System.Drawing.Point(57, 78);
+            this.mailP.Name = "mailP";
+            this.mailP.Size = new System.Drawing.Size(156, 20);
+            this.mailP.TabIndex = 40;
+            // 
+            // adresseP
+            // 
+            this.adresseP.Location = new System.Drawing.Point(57, 124);
+            this.adresseP.Name = "adresseP";
+            this.adresseP.Size = new System.Drawing.Size(156, 20);
+            this.adresseP.TabIndex = 41;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(12, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(240, 20);
+            this.label1.TabIndex = 42;
+            this.label1.Text = "E-mail du fournisseur";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(12, 101);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(240, 20);
+            this.label2.TabIndex = 43;
+            this.label2.Text = "Adresse du fournisseur";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.ImageAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.label3.Location = new System.Drawing.Point(12, 147);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(240, 20);
+            this.label3.TabIndex = 44;
+            this.label3.Text = "Site web du fournisseur";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // webP
+            // 
+            this.webP.Location = new System.Drawing.Point(57, 170);
+            this.webP.Name = "webP";
+            this.webP.Size = new System.Drawing.Size(156, 20);
+            this.webP.TabIndex = 45;
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.Location = new System.Drawing.Point(15, 215);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(80, 25);
+            this.cancelButton.TabIndex = 46;
+            this.cancelButton.Text = "Annuler";
+            this.cancelButton.UseVisualStyleBackColor = true;
+            this.cancelButton.Click += new System.EventHandler(this.cancelButton_Click);
+            // 
+            // OKButton
+            // 
+            this.OKButton.Location = new System.Drawing.Point(172, 215);
+            this.OKButton.Name = "OKButton";
+            this.OKButton.Size = new System.Drawing.Size(80, 25);
+            this.OKButton.TabIndex = 47;
+            this.OKButton.Text = "Valider";
+            this.OKButton.UseVisualStyleBackColor = true;
+            this.OKButton.Click += new System.EventHandler(this.OKButton_Click);
+            // 
+            // AddProvider
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(264, 261);
+            this.Controls.Add(this.OKButton);
+            this.Controls.Add(this.cancelButton);
+            this.Controls.Add(this.webP);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.adresseP);
+            this.Controls.Add(this.mailP);
+            this.Controls.Add(this.nameP);
+            this.Controls.Add(this.criticalityLabel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "AddProvider";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "AddProvider";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label criticalityLabel;
+        private System.Windows.Forms.TextBox nameP;
+        private System.Windows.Forms.TextBox mailP;
+        private System.Windows.Forms.TextBox adresseP;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox webP;
+        private System.Windows.Forms.Button cancelButton;
+        private System.Windows.Forms.Button OKButton;
+    }
+}
--- PT_Camping/View/UserControls/ProvidersUserControl.Designer.cs
+++ PT_Camping/View/UserControls/ProvidersUserControl.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace PT_Camping
+namespace PT_Camping
 {
     partial class ProvidersUserControl
     {
@@ -28,10 +28,273 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ProvidersUserControl));
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.ProvList = new System.Windows.Forms.ListView();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.addTextBox = new System.Windows.Forms.TextBox();
+            this.contactButton = new System.Windows.Forms.Button();
+            this.MailTextBox = new System.Windows.Forms.TextBox();
+            this.WebTextBox = new System.Windows.Forms.TextBox();
+            this.idTextBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.idLabel = new System.Windows.Forms.Label();
+            this.editButton = new System.Windows.Forms.Button();
+            this.deleteButton = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.newP = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // logoutButton
+            // 
+            this.logoutButton.FlatAppearance.BorderColor = System.Drawing.Color.RoyalBlue;
+            this.logoutButton.FlatAppearance.BorderSize = 0;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 5;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 5F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 40F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 40F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 5F));
+            this.tableLayoutPanel1.Controls.Add(this.ProvList, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.panel1, 3, 1);
+            this.tableLayoutPanel1.Controls.Add(this.newP, 1, 2);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 50);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 4;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 8F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 72F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 15F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 5F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(800, 550);
+            this.tableLayoutPanel1.TabIndex = 22;
+            // 
+            // ProvList
+            // 
+            this.ProvList.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ProvList.FullRowSelect = true;
+            this.ProvList.GridLines = true;
+            this.ProvList.Location = new System.Drawing.Point(43, 47);
+            this.ProvList.Name = "ProvList";
+            this.ProvList.Scrollable = false;
+            this.ProvList.Size = new System.Drawing.Size(314, 390);
+            this.ProvList.TabIndex = 0;
+            this.ProvList.UseCompatibleStateImageBehavior = false;
+            this.ProvList.SelectedIndexChanged += new System.EventHandler(this.ProvList_SelectedIndexChanged_1);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.addTextBox);
+            this.panel1.Controls.Add(this.contactButton);
+            this.panel1.Controls.Add(this.MailTextBox);
+            this.panel1.Controls.Add(this.WebTextBox);
+            this.panel1.Controls.Add(this.idTextBox);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.idLabel);
+            this.panel1.Controls.Add(this.editButton);
+            this.panel1.Controls.Add(this.deleteButton);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(443, 47);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(314, 390);
+            this.panel1.TabIndex = 1;
+            // 
+            // addTextBox
+            // 
+            this.addTextBox.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.addTextBox.Location = new System.Drawing.Point(160, 157);
+            this.addTextBox.Multiline = true;
+            this.addTextBox.Name = "addTextBox";
+            this.addTextBox.ReadOnly = true;
+            this.addTextBox.Size = new System.Drawing.Size(120, 40);
+            this.addTextBox.TabIndex = 22;
+            // 
+            // contactButton
+            // 
+            this.contactButton.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.contactButton.AutoSize = true;
+            this.contactButton.Location = new System.Drawing.Point(94, 342);
+            this.contactButton.Name = "contactButton";
+            this.contactButton.Size = new System.Drawing.Size(130, 30);
+            this.contactButton.TabIndex = 21;
+            this.contactButton.Text = "Contacter";
+            this.contactButton.UseVisualStyleBackColor = true;
+            this.contactButton.Click += new System.EventHandler(this.contactButton_Click);
+            // 
+            // MailTextBox
+            // 
+            this.MailTextBox.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.MailTextBox.Location = new System.Drawing.Point(160, 203);
+            this.MailTextBox.Name = "MailTextBox";
+            this.MailTextBox.ReadOnly = true;
+            this.MailTextBox.Size = new System.Drawing.Size(120, 20);
+            this.MailTextBox.TabIndex = 20;
+            // 
+            // WebTextBox
+            // 
+            this.WebTextBox.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.WebTextBox.Location = new System.Drawing.Point(160, 229);
+            this.WebTextBox.Name = "WebTextBox";
+            this.WebTextBox.ReadOnly = true;
+            this.WebTextBox.Size = new System.Drawing.Size(120, 20);
+            this.WebTextBox.TabIndex = 19;
+            // 
+            // idTextBox
+            // 
+            this.idTextBox.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.idTextBox.Location = new System.Drawing.Point(160, 128);
+            this.idTextBox.Name = "idTextBox";
+            this.idTextBox.ReadOnly = true;
+            this.idTextBox.Size = new System.Drawing.Size(120, 20);
+            this.idTextBox.TabIndex = 17;
+            // 
+            // label4
+            // 
+            this.label4.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(28, 232);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(120, 13);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "Site Web du fournisseur";
+            // 
+            // label3
+            // 
+            this.label3.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(28, 206);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(104, 13);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "E-mail du fournisseur";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(28, 160);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(114, 13);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Adresse du fournisseur";
+            // 
+            // idLabel
+            // 
+            this.idLabel.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.idLabel.AutoSize = true;
+            this.idLabel.Location = new System.Drawing.Point(28, 131);
+            this.idLabel.Name = "idLabel";
+            this.idLabel.Size = new System.Drawing.Size(98, 13);
+            this.idLabel.TabIndex = 13;
+            this.idLabel.Text = "Nom du fournisseur";
+            // 
+            // editButton
+            // 
+            this.editButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.editButton.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("editButton.BackgroundImage")));
+            this.editButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.editButton.Location = new System.Drawing.Point(278, 46);
+            this.editButton.Name = "editButton";
+            this.editButton.Size = new System.Drawing.Size(30, 30);
+            this.editButton.TabIndex = 12;
+            this.editButton.UseVisualStyleBackColor = true;
+            this.editButton.Click += new System.EventHandler(this.editButton_Click);
+            // 
+            // deleteButton
+            // 
+            this.deleteButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.deleteButton.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("deleteButton.BackgroundImage")));
+            this.deleteButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.deleteButton.Location = new System.Drawing.Point(242, 46);
+            this.deleteButton.Name = "deleteButton";
+            this.deleteButton.Size = new System.Drawing.Size(30, 30);
+            this.deleteButton.TabIndex = 11;
+            this.deleteButton.UseVisualStyleBackColor = true;
+            this.deleteButton.Click += new System.EventHandler(this.deleteButton_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.RoyalBlue;
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(312, 40);
+            this.panel2.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(79, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(149, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Détails fournisseurs";
+            // 
+            // newP
+            // 
+            this.newP.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.newP.Location = new System.Drawing.Point(140, 461);
+            this.newP.Name = "newP";
+            this.newP.Size = new System.Drawing.Size(120, 40);
+            this.newP.TabIndex = 2;
+            this.newP.Text = "Nouveau founisseur";
+            this.newP.UseVisualStyleBackColor = true;
+            this.newP.Click += new System.EventHandler(this.newP_Click);
+            // 
+            // ProvidersUserControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "ProvidersUserControl";
+            this.Controls.SetChildIndex(this.tableLayoutPanel1, 0);
+            this.Controls.SetChildIndex(this.appBarTitle, 0);
+            this.Controls.SetChildIndex(this.logoutButton, 0);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.ListView ProvList;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button deleteButton;
+        private System.Windows.Forms.Button editButton;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label idLabel;
+        private System.Windows.Forms.TextBox idTextBox;
+        private System.Windows.Forms.TextBox MailTextBox;
+        private System.Windows.Forms.TextBox WebTextBox;
+        private System.Windows.Forms.Button contactButton;
+        private System.Windows.Forms.TextBox addTextBox;
+        private System.Windows.Forms.Button newP;
     }
 }
