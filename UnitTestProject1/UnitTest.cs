afcd2afcb65cf1f6c28d249477f0a3d58b625652
yberwit <yberwit@gmail.com>
2017-03-16 11:00:51 +0100

    Unit test with dataBase connection works

---
+++ UnitTestProject1/UnitTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PT_Camping;
+using PT_Camping.Model;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class LoginToolsTests
+    {
+        [TestMethod]
+        public void checkConnectionTest()
+        {
+            LoginTools login = new LoginTools();
+            login.Login = "mcampo";
+            login.HashedPassword = LoginTools.sha256_hash("0000");
+            login.checkConnection();
+        }
+    }
+
+    [TestClass]
+    public class StockUserControlTest
+    {
+
+    }
+}
