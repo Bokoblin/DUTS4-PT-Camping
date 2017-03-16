a5bbf70d8cb68bf3421d385ad523873467148797
yberwit <yberwit@gmail.com>
2017-03-16 11:33:31 +0100

    Unit test loginTools end

--- UnitTestProject1/UnitTest.cs
+++ UnitTestProject1/UnitTest.cs
@@ -9,18 +9,38 @@ namespace UnitTestProject
     public class LoginToolsTests
     {
         [TestMethod]
-        public void checkConnectionTest()
+        public void checkConnectionSuccessfulTest()
         {
             LoginTools login = new LoginTools();
             login.Login = "mcampo";
             login.HashedPassword = LoginTools.sha256_hash("0000");
-            login.checkConnection();
+            Assert.IsTrue(login.checkConnection());
         }
-    }
-
-    [TestClass]
-    public class StockUserControlTest
-    {
-
+        [TestMethod]
+        public void checkConnectionFailedTest()
+        {
+            LoginTools login = new LoginTools();
+            login.Login = "existe";
+            login.HashedPassword = LoginTools.sha256_hash("false");
+            Assert.IsFalse(login.checkConnection());
+        }
+        [TestMethod]
+        public void checkConnectionWithoutPermissionTest()
+        {
+            LoginTools login = new LoginTools();
+            login.Login = "sfzag";
+            login.HashedPassword = LoginTools.sha256_hash("1234");
+            Assert.IsFalse(login.checkConnection());
+        }
+        [TestMethod]
+        public void sha256SuccessfulTest()
+        {
+            Assert.IsTrue(LoginTools.sha256_hash("lesha").Equals(LoginTools.sha256_hash("lesha")));
+        }
+        [TestMethod]
+        public void sha256FailedTest()
+        {
+            Assert.IsFalse(LoginTools.sha256_hash("unpremiersha").Equals(LoginTools.sha256_hash("unautresha")));
+        }
     }
 }
