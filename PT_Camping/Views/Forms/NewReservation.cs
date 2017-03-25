b9b0592d2072fc192aaa71ecf49f702f9d173cc3
ajacquot <jacquot.alexandre97@orange.fr>
2017-03-25 14:15:07 +0100

    added new reservation form

---
+++ PT_Camping/Views/Forms/NewReservation.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using PT_Camping.Model;
+
+namespace PT_Camping.Views.Forms
+{
+    public partial class NewReservation : Form
+    {
+        private DataBase _db;
+
+        public NewReservation()
+        {
+            InitializeComponent();
+            _db = new DataBase();
+            foreach (Personne person in _db.Personne)
+            {
+                reservationHolderComboBox.Items.Add(person.Nom_Personne + " " + person.Prenom_Personne);
+                reservationHolderComboBox.AutoCompleteCustomSource.Add(person.Nom_Personne + " " + person.Prenom_Personne);
+            }
+        }
+
+        public NewReservation(int codePerson): this()
+        {
+            Personne client = _db.Personne.Find(codePerson);
+            if (client != null)
+            {
+                reservationHolderComboBox.SelectedText = client.Nom_Personne + " " + client.Prenom_Personne;
+            }
+        }
+
+        private void addLocationButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void deleteLocationButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void addLodgerButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void removeLodgerButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void addReservationButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void reservationHolderComboBox_TextChanged(object sender, EventArgs e)
+        {
+            /*String text = reservationHolderComboBox.SelectedText;
+            reservationHolderComboBox.Items.Clear();
+            foreach (Personne person in _db.Personne.Where(a => a.Nom_Personne.Contains(text) || a.Prenom_Personne.Contains(text)))
+            {
+                reservationHolderComboBox.Items.Add(person.Nom_Personne + " " + person.Prenom_Personne);
+            }
+            reservationHolderComboBox.DroppedDown = true;
+            reservationHolderComboBox.Refresh();
+            reservationHolderComboBox.Focus();
+            if (reservationHolderComboBox.Text.Length > 0)
+            {
+                reservationHolderComboBox.SelectionStart = reservationHolderComboBox.Text.Length;
+                reservationHolderComboBox.SelectionLength = 0;
+            }*/
+        }
+    }
+}
--- PT_Camping/Views/Forms/Reservations.Designer.cs
+++ PT_Camping/Views/Forms/Reservations.Designer.cs
@@ -34,16 +34,20 @@
             this.reservationsList = new System.Windows.Forms.ListView();
             this.beginDateColumnHeader = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.endDateColumnHeader = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.locationNameColumnHeader = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.ispayedColumnHeader = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.nbPersColumnHeader = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.ispayedColumnHeader = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.label1 = new System.Windows.Forms.Label();
+            this.locationsList = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lodgerList = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // newReservationButton
             // 
             this.newReservationButton.Location = new System.Drawing.Point(185, 365);
             this.newReservationButton.Name = "newReservationButton";
-            this.newReservationButton.Size = new System.Drawing.Size(162, 23);
+            this.newReservationButton.Size = new System.Drawing.Size(162, 32);
             this.newReservationButton.TabIndex = 1;
             this.newReservationButton.Text = "Nouvelle Réservation";
             this.newReservationButton.UseVisualStyleBackColor = true;
@@ -53,7 +57,7 @@
             // 
             this.deleteReservationButton.Location = new System.Drawing.Point(42, 365);
             this.deleteReservationButton.Name = "deleteReservationButton";
-            this.deleteReservationButton.Size = new System.Drawing.Size(88, 23);
+            this.deleteReservationButton.Size = new System.Drawing.Size(88, 32);
             this.deleteReservationButton.TabIndex = 2;
             this.deleteReservationButton.Text = "Supprimer";
             this.deleteReservationButton.UseVisualStyleBackColor = true;
@@ -63,7 +67,7 @@
             // 
             this.factureButton.Location = new System.Drawing.Point(409, 365);
             this.factureButton.Name = "factureButton";
-            this.factureButton.Size = new System.Drawing.Size(171, 23);
+            this.factureButton.Size = new System.Drawing.Size(171, 32);
             this.factureButton.TabIndex = 3;
             this.factureButton.Text = "Télécharger la facture";
             this.factureButton.UseVisualStyleBackColor = true;
@@ -74,14 +78,15 @@
             this.reservationsList.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
             this.beginDateColumnHeader,
             this.endDateColumnHeader,
-            this.locationNameColumnHeader,
             this.nbPersColumnHeader,
             this.ispayedColumnHeader});
-            this.reservationsList.Location = new System.Drawing.Point(13, 13);
+            this.reservationsList.Location = new System.Drawing.Point(13, 34);
+            this.reservationsList.MultiSelect = false;
             this.reservationsList.Name = "reservationsList";
-            this.reservationsList.Size = new System.Drawing.Size(615, 346);
+            this.reservationsList.Size = new System.Drawing.Size(405, 325);
             this.reservationsList.TabIndex = 4;
             this.reservationsList.UseCompatibleStateImageBehavior = false;
+            this.reservationsList.ItemSelectionChanged += new System.Windows.Forms.ListViewItemSelectionChangedEventHandler(this.reservationsList_ItemSelectionChanged);
             // 
             // beginDateColumnHeader
             // 
@@ -91,25 +96,72 @@
             // 
             this.endDateColumnHeader.Text = "Date Fin";
             // 
-            // locationNameColumnHeader
+            // nbPersColumnHeader
             // 
-            this.locationNameColumnHeader.Text = "Nom Emplacement";
+            this.nbPersColumnHeader.DisplayIndex = 3;
+            this.nbPersColumnHeader.Text = "Nombre personnes";
             // 
             // ispayedColumnHeader
             // 
-            this.ispayedColumnHeader.DisplayIndex = 3;
+            this.ispayedColumnHeader.DisplayIndex = 2;
             this.ispayedColumnHeader.Text = "Est payée";
             // 
-            // nbPersColumnHeader
+            // label1
             // 
-            this.nbPersColumnHeader.DisplayIndex = 4;
-            this.nbPersColumnHeader.Text = "Nombre personnes";
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(425, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(168, 17);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Liste des emplacements :";
+            // 
+            // locationsList
+            // 
+            this.locationsList.FormattingEnabled = true;
+            this.locationsList.ItemHeight = 16;
+            this.locationsList.Location = new System.Drawing.Point(425, 34);
+            this.locationsList.Name = "locationsList";
+            this.locationsList.Size = new System.Drawing.Size(203, 148);
+            this.locationsList.TabIndex = 6;
+            this.locationsList.SelectedIndexChanged += new System.EventHandler(this.locationsList_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(425, 183);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(138, 17);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Liste des locataires :";
+            // 
+            // lodgerList
+            // 
+            this.lodgerList.FormattingEnabled = true;
+            this.lodgerList.ItemHeight = 16;
+            this.lodgerList.Location = new System.Drawing.Point(425, 210);
+            this.lodgerList.Name = "lodgerList";
+            this.lodgerList.Size = new System.Drawing.Size(203, 148);
+            this.lodgerList.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 11);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(171, 17);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Liste des réservations de ";
             // 
             // Reservations
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(640, 400);
+            this.ClientSize = new System.Drawing.Size(640, 409);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lodgerList);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.locationsList);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.reservationsList);
             this.Controls.Add(this.factureButton);
             this.Controls.Add(this.deleteReservationButton);
@@ -117,6 +169,7 @@
             this.Name = "Reservations";
             this.Text = "Reservations";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -127,8 +180,12 @@
         private System.Windows.Forms.ListView reservationsList;
         private System.Windows.Forms.ColumnHeader beginDateColumnHeader;
         private System.Windows.Forms.ColumnHeader endDateColumnHeader;
-        private System.Windows.Forms.ColumnHeader locationNameColumnHeader;
         private System.Windows.Forms.ColumnHeader ispayedColumnHeader;
         private System.Windows.Forms.ColumnHeader nbPersColumnHeader;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox locationsList;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox lodgerList;
+        private System.Windows.Forms.Label label3;
     }
 }
