0563c46c261b473c709849a6672849274e61fc69
Arthur Jolivet <jolivet.arthur@gmail.com>
2017-03-25 15:35:33 +0100

    Added GraphicLocation class tests

---
+++ UnitTestProject1/LoginToolsTests.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PT_Camping.Model;
+
+namespace UnitTestProject
+{
+    /// <summary>
+    /// This test class is used to test the PT_Camping.Model.LoginTools class.
+    /// 
+    /// </summary>
+    /// Authors : Yonnel
+    /// Since : 16/02/17
+    [TestClass]
+    public class LoginToolsTests
+    {
+        [TestMethod]
+        public void CheckConnectionSuccessfulTest()
+        {
+            LoginTools.Login = "test-connection";
+            LoginTools.HashedPassword = LoginTools.Sha256_hash("success");
+            Assert.IsTrue(LoginTools.CheckConnection());
+        }
+
+
+        [TestMethod]
+        public void CheckConnectionFailedTest()
+        {
+            LoginTools.Login = "existe";
+            LoginTools.HashedPassword = LoginTools.Sha256_hash("false");
+            Assert.IsFalse(LoginTools.CheckConnection());
+        }
+
+
+        [TestMethod]
+        public void CheckConnectionFailedWithoutPermissionTest()
+        {
+            LoginTools.Login = "test-licencie";
+            LoginTools.HashedPassword = LoginTools.Sha256_hash("1234");
+            Assert.IsFalse(LoginTools.CheckConnection());
+        }
+
+
+        [TestMethod]
+        public void Sha256SuccessfulTest()
+        {
+            Assert.IsTrue(LoginTools.Sha256_hash("lesha").Equals(LoginTools.Sha256_hash("lesha")));
+        }
+
+
+        [TestMethod]
+        public void Sha256FailedTest()
+        {
+            Assert.IsFalse(LoginTools.Sha256_hash("unpremiersha").Equals(LoginTools.Sha256_hash("unautresha")));
+        }
+    }
+}
+
