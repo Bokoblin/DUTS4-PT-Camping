dbaf066143c39fbc91694e80d421a606640a7367
Arthur Jolivet <arthur.jolivet@etu.u-bordeaux.fr>
2017-03-25 15:56:53 +0100

    Replaced VSTests by NUnit for travis compat

--- UnitTestProject1/LoginToolsTests.cs
+++ UnitTestProject1/LoginToolsTests.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using PT_Camping.Model;
+﻿using PT_Camping.Model;
+using TestClass = NUnit.Framework.TestFixtureAttribute;
+using TestMethod = NUnit.Framework.TestAttribute;
+using Assert = NUnit.Framework.Assert;
+
 
 namespace UnitTestProject
 {
